0d2f4b5640f35979e3c7dc129af31f2d5a425308
nambak2218-debug <nambak2218@gmail.com>
2026-07-09 17:14:23 +0900

    Update telegram_service.py

--- services/telegram_service.py
+++ services/telegram_service.py
@@ -1,1 +1,19 @@
-#Telegram Service
+from telegram import Bot
+
+
+class TelegramService:
+
+    def __init__(self, token):
+
+        self.bot = Bot(token)
+
+    async def send(
+        self,
+        chat_id,
+        message
+    ):
+
+        await self.bot.send_message(
+            chat_id=chat_id,
+            text=message
+        )
