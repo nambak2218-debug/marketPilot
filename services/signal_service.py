5d3fca4ced23560c6a4adea2dc3d2337b113ede6
nambak2218-debug <nambak2218@gmail.com>
2026-07-09 17:40:29 +0900

    Update signal_service.py

--- services/signal_service.py
+++ services/signal_service.py
@@ -4,35 +4,19 @@ class SignalService:
     def decide(score):
 
         if score >= 80:
-            return {
-                "emoji": "🟢",
-                "action": "강한 레버리지",
-                "color": "GREEN"
-            }
+
+            return "🟢 강한 레버리지"
 
         if score >= 65:
-            return {
-                "emoji": "🟢",
-                "action": "레버리지",
-                "color": "GREEN"
-            }
+
+            return "🟢 레버리지"
 
         if score >= 45:
-            return {
-                "emoji": "🟡",
-                "action": "관망",
-                "color": "YELLOW"
-            }
+
+            return "🟡 관망"
 
         if score >= 25:
-            return {
-                "emoji": "🔴",
-                "action": "인버스",
-                "color": "RED"
-            }
 
-        return {
-            "emoji": "🔴",
-            "action": "강한 인버스",
-            "color": "RED"
-        }
+            return "🔴 인버스"
+
+        return "🔴 강한 인버스"
