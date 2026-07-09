8ce1bd790cd5a4a22fcf75bc44fe89154c0d27bd
nambak2218-debug <nambak2218@gmail.com>
2026-07-09 16:18:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("MarketPilot Start")
+import os
+from telegram import Bot
+
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+bot = Bot(token=TOKEN)
+
+bot.send_message(
+    chat_id=CHAT_ID,
+    text="🚀 MarketPilot v1.0\n\nGitHub Actions 연결 성공!"
+)
+
+print("Done")
