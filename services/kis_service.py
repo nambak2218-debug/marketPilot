41987059d1545cd19ebe105f4173ce020f7b2331
nambak2218-debug <nambak2218@gmail.com>
2026-07-09 22:40:24 +0900

    Update kis_service.py

--- services/kis_service.py
+++ services/kis_service.py
@@ -5,11 +5,8 @@ import requests
 class KISService:
 
     def __init__(self):
-
         self.base_url = os.environ["KIS_BASE_URL"]
-
         self.app_key = os.environ["KIS_APP_KEY"]
-
         self.app_secret = os.environ["KIS_APP_SECRET"]
 
     def get_access_token(self):
@@ -17,19 +14,15 @@ class KISService:
         url = f"{self.base_url}/oauth2/tokenP"
 
         body = {
-
             "grant_type": "client_credentials",
-
             "appkey": self.app_key,
-
             "appsecret": self.app_secret
-
         }
 
         response = requests.post(url, json=body)
 
         response.raise_for_status()
 
-        token = response.json()["access_token"]
+        data = response.json()
 
-        return token
+        return data["access_token"]
